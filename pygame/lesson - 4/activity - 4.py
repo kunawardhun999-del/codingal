45702d99d80c7a609b250f9547c551794192de4a
kunawardhun999-del <kunawardhun999@gmail.com>
2026-06-17 20:04:56 +0700

    activity4

--- pygame/lesson - 4/activity - 4.py
+++ pygame/lesson - 4/activity - 4.py
@@ -17,4 +17,24 @@ def open_file():
         txt_edit.insert(END, text)
     window.title(f'Condingals Text Editor - {filepath}')
 
- 
+def save_file():
+    filepath = asksaveasfilename(defaultextension='txt', filetypes=[('Text Files', '*.txt'), ('All Files', '*.*')])
+    if not filepath:
+        return
+    with open(filepath, 'w') as output_file:
+        text = txt_edit.get(1.0, END)
+        output_file.write(text)
+    window.title(f'Condingals Text Editor - {filepath}')
+
+txt_edit = Text(window)
+fr_buttons = Frame(window, relief=RAISED, bd=2)
+btn_open = Button(fr_buttons, text='Open', command=open_file)
+btn_save = Button(fr_buttons, text='Save As...', command=save_file)
+
+btn_open.grid(row=0, column=0, sticky='ew', padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky='ew', padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky='ns')
+txt_edit.grid(row=0, column=1, sticky='nsew')
+
+window.mainloop()
